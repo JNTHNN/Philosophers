dab209bd95250a1325b0d594dc13fbf4f139a020
Jonathan Gasparoly <jgasparo@fu-r9-p4.s19.be>
2024-01-25 14:28:59 +0100

    init project

---
+++ philo/main.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgasparo <jgasparo@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/25 13:28:37 by jgasparo          #+#    #+#             */
+/*   Updated: 2024/01/25 14:23:33 by jgasparo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./includes/philosophers.h"
+
+int	main(int argc, char **argv)
+{
+	if (argc == 6 || argc == 7)
+	{
+		if (argc == 7)
+		/* number_of_times_each_philosopher_must_eat */
+	}
+	
+}
