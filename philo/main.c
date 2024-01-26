3bb66fba04dd45aedda9522bcda341d940bdafa8
Jonathan Gasparoly <jgasparo@fu-r9-p4.s19.be>
2024-01-26 16:01:47 +0100

    init some shiet

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: jgasparo <jgasparo@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 13:28:37 by jgasparo          #+#    #+#             */
-/*   Updated: 2024/01/25 14:23:33 by jgasparo         ###   ########.fr       */
+/*   Updated: 2024/01/25 15:29:40 by jgasparo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,19 @@
 
 int	main(int argc, char **argv)
 {
-	if (argc == 6 || argc == 7)
-	{
-		if (argc == 7)
-		/* number_of_times_each_philosopher_must_eat */
-	}
+	int	number_of_philosophers;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	number_of_times_each_philosopher_must_eat;
 	
+	if (argc == 5 || argc == 6)
+	{
+		if (argc == 6)
+			number_of_times_each_philosopher_must_eat = argv[5];
+		number_of_philosophers = argv[1];
+		time_to_die = argv[2];
+		time_to_eat = argv[3];
+		time_to_sleep = argv[4];
+	}
 }
